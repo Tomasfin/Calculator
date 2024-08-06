95317c0d1ad8d5c60870bba8110311ebb738b7a2
Tomasfin <tomasfin22@gmail.com>
2024-08-06 08:20:06 +0800

    Fixed issue where program could take strings and treating the numbers as strings. Completed project

--- calculator.py
+++ calculator.py
@@ -10,25 +10,38 @@ def division(value1, value2):
 print('Welcome to the super simple terminal calculator!')
 result = False
 while result is False:
+
  value1 = input('Please enter a number\n')
- if int(value1) / 1:
-    result = True
-    while result is True:
-       operator = input('Choose an operator (*)(/)(+)(-)\n')
-       if operator in ['*','/','+','-']:
-          result = False
-          while result is False:
-           value2 = int(input('Choose the second number\n'))
-           if value2 / 1:
-              if operator is '*':
-                 print(multiply(value1, value2))
-              if operator is '/':
-                 print(division(value1,value2))
-              if operator is '+':
-                 print(addition(value1,value2))
-              if operator is '-':
-                 print(subtraction(value1,value2))
-              break
+ try:
+  if float(value1):
+   result = True
+ except ValueError:
+    result = False
+while result is True:
+ operator = input('Choose an operator (*)(/)(+)(-)\n')
+ if operator in [('*'),('/'),('+'),('-')]:
+   result = False
+ else:
+  result = True
+
+while result is False:
+ value2 = input('Choose the second number\n')
+ try:
+  if float(value1):
+   result = True
+ except ValueError:
+    result = False
+if operator is '*':
+ print('Answer: ',multiply(float(value1), float(value2)))
+if operator is '/':
+ print('Answer: ',division(float(value1), float(value2)))
+if operator is '+':
+  print('Answer: ',addition(float(value1), float(value2)))
+if operator is '-':
+  print('Answer: ',subtraction(float(value1), float(value2)))
+              
+
+    
 
    
         
